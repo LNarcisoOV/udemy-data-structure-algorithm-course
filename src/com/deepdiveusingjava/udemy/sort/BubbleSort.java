317a766c8aea82d25ecfd4e242c5277e81b6d2f3
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-04-08 10:46:14 -0300

    [SORT] Selection sort implementation.

--- src/com/deepdiveusingjava/udemy/sort/BubbleSort.java
+++ src/com/deepdiveusingjava/udemy/sort/BubbleSort.java
@@ -11,20 +11,23 @@ public class BubbleSort {
 		for (int i = 0; i < intArray.length; i++) {
 			System.out.println(intArray[i]);
 		}
-		
+
 		sort(intArray);
 
 		System.out.println("\nBubble sorted array: ");
 		for (int i = 0; i < intArray.length; i++) {
 			System.out.println(intArray[i]);
 		}
-		
-		System.out.println("\nOne of the most inefficient algorithms for sorting.\n"
-				+ "It's possible to see by the numbers of 'fors' in the code, \n"
-				+ "if the collection grows, the number of steps will grow too.\n"
-				+ "Big-O: O(N²);");
-		
 
+		System.out.println("\nOne of the most inefficient algorithms for sorting.\n"
+				+ "Bubble sort is a stable algorithm, because it will preserve the order of \n"
+				+ "the same elements, in this 'If': if (intArray[j] > intArray[j + 1]), \n"
+				+ "to be an unstable algorithm we will need to change this algorithm and put \n"
+				+ "'>=' instead of '>' in the 'If'. Change this comparsion will do \n"
+				+ "the algorithm swap elements with the same value.\n"
+				+ "It's possible to see by the numbers of 'fors' in the code, \n"
+				+ "if the collection grows, the number of steps will grow too.\n" 
+				+ "Big-O: O(N²);");
 	}
 
 	private static void sort(int[] intArray) {
---
+++ src/com/deepdiveusingjava/udemy/sort/SelectionSort.java
@@ -0,0 +1,51 @@
+package com.deepdiveusingjava.udemy.sort;
+
+import com.deepdiveusingjava.udemy.arrays.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int intArray[] = Arrays.createIntArray();
+
+		System.out.println("Unsorted array: ");
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+
+		sort(intArray);
+
+		System.out.println("\nSelection sorted array: ");
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+		
+		System.out.println("\n\nIt looks like a bubble sort algorithm, \nbut there are fewer swaps than bubble sort, \n"
+				+ "because it will compare the current value against the largest \nelement and passing only the "
+				+ "index with the biggest value \nto be replaced in the last unsorted index. \n"
+				+ "It's an unstable algorithm, so it will swap \nelements with that same value. \n"
+				+ "Big-O: O(N²);");
+	}
+
+	private static void sort(int[] intArray) {
+		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex >= 0; lastUnsortedIndex--) {
+			int largestElementIndex = 0;
+			for (int i = 0; i <= lastUnsortedIndex; i++) {
+				if (intArray[i] > intArray[largestElementIndex]) {
+					largestElementIndex = i;
+				}
+			}
+			swap(intArray, largestElementIndex, lastUnsortedIndex);
+		}
+	}
+
+	public static void swap(int array[], int largestElementIndex, int lastUnsortedIndex) {
+		if (largestElementIndex == lastUnsortedIndex) {
+			return;
+		}
+
+		int temp = array[largestElementIndex];
+		array[largestElementIndex] = array[lastUnsortedIndex];
+		array[lastUnsortedIndex] = temp;
+	}
+
+}
