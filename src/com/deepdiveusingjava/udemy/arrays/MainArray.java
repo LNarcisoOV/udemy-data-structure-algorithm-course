4531cfe16a5088e70ffaeaf01c5079a928898bea
Leonardo Narciso <lnarcisoov@gmail.com>
2019-04-15 11:50:53 -0300

    [ARRAY] Creating method to return a string array for video 42, radix sort challenge.

--- src/com/deepdiveusingjava/udemy/arrays/MainArray.java
+++ src/com/deepdiveusingjava/udemy/arrays/MainArray.java
@@ -7,65 +7,65 @@ public class MainArray {
 		System.out.println();
 		cons();
 		System.out.println();
-		System.out.println(" If the code doesn't require the loop, it means, has an index, it's gonna be a CONSTANT TIME.");
+		System.out.println(
+				" If the code doesn't require the loop, it means, has an index, it's gonna be a CONSTANT TIME.");
 		System.out.println(" If the code requires the loop, it means hasn't an index, it's gonna be a LINEAR TIME.");
 	}
-
 
 	private static void pros() {
 		System.out.println("PROS: ");
 		System.out.println(" You have to specify the size of the array because, in memory, it will \n"
-				+" reserve a continuous block for this array, so, all the values will be \n"
-				+" in this block, not scattered in different places in memory. \n"
-				+" Every element in the array will occupy the same amount of space in \n"
-				+" memory of the specified type, it works such for primitive types as \n"
-				+" objects because the objects array will store the reference of the \n"
-				+" object, not the object itself. \n"
-				+" If we know the index of the array, the time to retrieve or insert or update or delete \n"
-				+" the element will be the same regardless of the position of the element in the \n"
-				+" array because we can just do a simple mathematical formula to find  \n"
-				+" the element in the memory. \n"
-				+" In others words, we will need only 3 steps to retrieve this element. \n"
-				+" Formula: startAddressMemory + (indexOfArray * sizeOfTheTypeOrReferenceOfTheElement) \n"				
-				+"\n Big-O: It will be the best scenario ever: O(1) - Constant time. ");	
-		
+				+ " reserve a continuous block for this array, so, all the values will be \n"
+				+ " in this block, not scattered in different places in memory. \n"
+				+ " Every element in the array will occupy the same amount of space in \n"
+				+ " memory of the specified type, it works such for primitive types as \n"
+				+ " objects because the objects array will store the reference of the \n"
+				+ " object, not the object itself. \n"
+				+ " If we know the index of the array, the time to retrieve or insert or update or delete \n"
+				+ " the element will be the same regardless of the position of the element in the \n"
+				+ " array because we can just do a simple mathematical formula to find  \n"
+				+ " the element in the memory. \n"
+				+ " In others words, we will need only 3 steps to retrieve this element. \n"
+				+ " Formula: startAddressMemory + (indexOfArray * sizeOfTheTypeOrReferenceOfTheElement) \n"
+				+ "\n Big-O: It will be the best scenario ever: O(1) - Constant time. ");
+
 		System.out.println("\nARRAY: ");
 
 		int[] intArray = createIntArray();
-		
+
 		for (int i = 0; i < intArray.length; i++) {
 			System.out.println(intArray[i]);
 		}
 		System.out.println("--------------------------------------------------------------------------------");
 	}
-	
+
 	private static void cons() {
 		System.out.println("CONS: ");
 		System.out.println(" If you don't know the index of the element in the array, it will be \n"
-		+" more difficult to retrieve the element, because you will have to go \n"
-		+" through the array until you find the element you want. \n"
-		+" If we want to retrieve the element with the value equals to -22, \n"
-		+" we will have to go through to the whole array as following example below. \n"
-		+" If we want to delete a specific element and shift the position of all others elements, \n"
-		+" we will have to loop and decrease each position of each element after that deleted element. \n"
-		+"\n Big-O: It will be the worst scenario ever: O(N) - Linear time. ");
-		
+				+ " more difficult to retrieve the element, because you will have to go \n"
+				+ " through the array until you find the element you want. \n"
+				+ " If we want to retrieve the element with the value equals to -22, \n"
+				+ " we will have to go through to the whole array as following example below. \n"
+				+ " If we want to delete a specific element and shift the position of all others elements, \n"
+				+ " we will have to loop and decrease each position of each element after that deleted element. \n"
+				+ "\n Big-O: It will be the worst scenario ever: O(N) - Linear time. ");
+
 		System.out.println("\nINDEX: ");
-		
+
 		int[] intArray = createIntArray();
 
 		int index = -1;
 		for (int i = 0; i < intArray.length; i++) {
-			if(intArray[i] == -22){
+			if (intArray[i] == -22) {
 				index = i;
 				break;
 			}
 		}
-		
+
 		System.out.println("Index for '-22' value is: " + index);
 		System.out.println("--------------------------------------------------------------------------------");
 	}
-	
+
 	public static int[] createIntArray() {
 		int[] intArray = new int[7];
 
@@ -79,10 +79,10 @@ public class MainArray {
 
 		return intArray;
 	}
-	
-	public static int[] createIntArrayForCountingSort(){
+
+	public static int[] createIntArrayForCountingSort() {
 		int[] intArray = new int[10];
-		
+
 		intArray[0] = 2;
 		intArray[1] = 5;
 		intArray[2] = 9;
@@ -93,21 +93,33 @@ public class MainArray {
 		intArray[7] = 10;
 		intArray[8] = 4;
 		intArray[9] = 3;
-		
+
 		return intArray;
 	}
-	
-	public static int[] createIntArrayForRadixSort(){
+
+	public static int[] createIntArrayForRadixSort() {
 		int[] intArray = new int[6];
-		
+
 		intArray[0] = 4725;
 		intArray[1] = 4586;
 		intArray[2] = 1330;
 		intArray[3] = 8792;
 		intArray[4] = 1594;
 		intArray[5] = 5729;
-		
+
 		return intArray;
+	}
+
+	public static String[] createIntArrayForRadixSortChallenge() {
+		String[] stringArray = new String[5];
+
+		stringArray[0] = "bcdef";
+		stringArray[1] = "dbaqc";
+		stringArray[2] = "abcde";
+		stringArray[3] = "omadd";
+		stringArray[4] = "bbbbb";
+
+		return stringArray;
 	}
 
 }
