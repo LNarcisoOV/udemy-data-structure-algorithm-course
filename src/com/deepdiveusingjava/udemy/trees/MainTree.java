7de1e34b49d464d6743c471123368544e6d6d956
Leonardo Narciso Oliveira do Vale <leonardo.vale@marquise.com.br>
2019-07-08 15:18:23 -0300

    [TREES] Theory.

--- src/com/deepdiveusingjava/udemy/trees/MainTree.java
+++ src/com/deepdiveusingjava/udemy/trees/MainTree.java
@@ -2,6 +2,23 @@ package com.deepdiveusingjava.udemy.trees;
 
 public class MainTree {
 	public static void main(String args[]) {
+		System.out.println("::::: TREE ::::: \n");
 
+		System.out.println("");
+		System.out.println("- Hierarchical data structure.");
+		System.out.println("- Each field in the tree is called 'Node'.");
+		System.out.println("- Each node can have a lot of childrens, but each children ");
+		System.out.println("can have one, and only one parent.");
+		System.out.println("- The first node of the tree is called 'ROOT' and it doesn't");
+		System.out.println("have parent.");
+		System.out.println("- Every tree can have one and only one root node.");
+		System.out.println("- Trees are ideal when the things contains things or when things");
+		System.out.println("descend from other things.");
+		System.out.println("- Java class hierarchy is an example of tree.");
+		System.out.println("- A node without children is called 'leaf node'.");
+		System.out.println("- If a tree has only one node, is called a singleton tree, because");
+		System.out.println("this tree has only the root node.");
+		System.out.println("- Every node with a descendants(childrens) are called Subtree.");
+		
 	}
 }
