757ee50ee2cfbc3825f2a3b266c1e49bc37679fe
Leonardo Narciso <lnarcisoov@gmail.com>
2019-07-09 17:09:34 -0300

    [HEAPS] Added package and main class.

---
+++ src/com/deepdiveusingjava/udemy/heaps/MainHeap.java
@@ -0,0 +1,8 @@
+package com.deepdiveusingjava.udemy.heaps;
+
+public class MainHeap {
+	public static void main(String args[]) {
+		System.out.println("::::: HEAPS ::::: ");
+		System.out.println("");
+	}
+}
