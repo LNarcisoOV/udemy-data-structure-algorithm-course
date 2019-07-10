8372c5b95539e9b75bf48e6b04f6935c0dfddc81
Leonardo Narciso <lnarcisoov@gmail.com>
2019-07-10 09:56:05 -0300

    [HEAPS] Theory.

--- src/com/deepdiveusingjava/udemy/heaps/MainHeap.java
+++ src/com/deepdiveusingjava/udemy/heaps/MainHeap.java
@@ -4,5 +4,17 @@ public class MainHeap {
 	public static void main(String args[]) {
 		System.out.println("::::: HEAPS ::::: ");
 		System.out.println("");
+		System.out.println("- Special type of binary tree.");
+		System.out.println("- A complete binary tree.");
+		System.out.println("- Must satisfy the heap porperty.");
+		System.out.println("- Max heap: Every parent is greater than or equal to its children.");
+		System.out.println("- Min heap: Every parent is less than or equal to its children.");
+		System.out.println("- Each children are added from left to right.");
+		System.out.println("- Usually implemented as arrays.");
+		System.out.println("- THE MINIMUM OR MAXIMUM VALUES WILL ALWAYS AT THE TOP OF ");
+		System.out.println("THE TREE - THE ADVANTAGE OF USING HEAP.");
+		System.out.println("- HEAPFY: process of converting a binary tree into a heap,");
+		System.out.println("this often has to be done after insertion or deletion.");
+		System.out.println("- No required ordering between siblings.");
 	}
 }
